b2827b85c299dcd63363e6d3016e9a4ce275404c
MoritzAlexanderHeld <moritz.held0027@stud.hkr.se>
2024-05-28 01:13:59 +0200

    Basic setup of Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  pathPrefix: "/my-gatsby-site",
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
